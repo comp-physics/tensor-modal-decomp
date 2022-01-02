cf969deeeffa707a1878913b2a88f201ea85ac6f
Florian Schaefer <flotosch@gmail.com>
2022-01-02 15:55:03 -0500

    simplify interfaces,  spod still not finished

--- ica_julia/decompositions.jl
+++ ica_julia/decompositions.jl
@@ -1,22 +1,47 @@
 # Input variables:
-# p: The input three-way tensor 
+# p: The input three-way tensor, first two dimensions are spatial, third is time
 # k: The number of components
 # ds_i: The offset for subsampling on first dim
 # ds_j: The offset for subsampling on second dim
 
 function compute_ica(p::AbstractArray{<:Any,3}, k, ds_i=1, ds_j=1)
-    i_inds = 1 : ds_i : size(p, 2)
-    j_inds = 1 : ds_j : size(p, 3)
-    p = p[:, i_inds, j_inds]
-    p = Matrix(transpose(reshape(p, (size(p, 1), :))))
-    return fit(ICA, p, k, maxiter=10000).W, i_inds, j_inds
+    i_inds = 1 : ds_i : size(p, 1)
+    j_inds = 1 : ds_j : size(p, 2)
+    p = p[i_inds, j_inds, :]
+    @show size(p)
+    p = reshape(p, (:, size(p, 3)))
+    @show size(p)
+    modes = fit(ICA, p, k; maxiter=10000, tol=1e-3).W
+    modes = reshape(modes, (length(i_inds), length(j_inds), size(modes, 2)))
+    return modes, i_inds, j_inds
 end 
 
 function compute_pod_svd(p::AbstractArray{<:Any, 3}, ds_i=1, ds_j=1)
-    i_inds = 1 : ds_i : size(p, 2)
-    j_inds = 1 : ds_j : size(p, 3)
-    p = p[:, i_inds, j_inds]
+    i_inds = 1 : ds_i : size(p, 1)
+    j_inds = 1 : ds_j : size(p, 2)
+    p = p[i_inds, j_inds, :]
+    p = reshape(p, (:, size(p, 3)))
+    @show size(p)
+    # returns a number of modes times number of dof matrix
+    modes = POD(p)[1].modes
+    @show size(modes)
+    # reshape the modes again so that they return the 
+    modes = reshape(modes, (length(i_inds), length(j_inds), size(modes, 2)))
+    return modes, i_inds, j_inds
+end
+
+# STILL NOT FINISHED! 
+function compute_spod_svd(p::AbstractArray{<:Any, 3}, ds_i=1, ds_j=1)
+    i_inds = 1 : ds_i : size(p, 1)
+    j_inds = 1 : ds_j : size(p, 2)
+    p = p[i_inds, j_inds, :]
+    # Applying time-domain fft
+    p = fft(p, (1))
     p = Matrix(transpose(reshape(p, (size(p, 1), :))))
     @show size(p)
-    return POD(p)[1].modes, i_inds, j_inds
+    modes = POD(fftp)[1].modes
+    # reshape the modes again so that they return the 
+    fftmodes = reshape(modes, (length(i_inds), length(j_inds), size(modes, 2)))
+    modes = ifft(fftmodes, (1,2))
+    return modes, i_inds, j_inds
 end
--- ica_julia/main.jl
+++ ica_julia/main.jl
@@ -1,4 +1,5 @@
 using MAT
+
 using MultivariateStats
 using GLMakie
 using ProperOrthogonalDecomposition
@@ -7,6 +8,8 @@ include("decompositions.jl")
 
 data = matread("../spod/jet_data/jetLES.mat")
 p = data["p"]
+# switches the spatial dimensions to the leading dimension
+p = permutedims(p, [2, 3, 1])
 
 # The fastica algorithm used by the packages doesn't seem to converge
 # reliably. Convergence seems to depend on the initialization, so we try 
@@ -17,37 +20,39 @@ j_indices = nothing
 k = 1
 while isnothing(ica)
     try 
-        global ica, i_indices, j_indices = compute_ica(p, 16, 1, 1)
+        global ica, i_indices, j_indices = compute_ica(p, 20, 1, 1)
     catch
         @warn "ica not converged in $k-th attempt"
         global k = k + 1
     end
+    if k >20 break end
 end
 
 # This becomes a three-way tensor; the k-th component is given by components[:, :, k]
-independent_components = reshape(ica, (length(i_indices), length(j_indices), size(ica, 2)))
+# now included in the ica implementation
+# independent_components = reshape(ica, (length(i_indices), length(j_indices), size(ica, 2)))
 
 # Plots the independent components
 figs_per_row = 4
 fig_ica = Figure()
-for (k, ind) in enumerate(CartesianIndices((figs_per_row, size(independent_components, 3)))[:])
-    if k <= size(independent_components, 3)
-        heatmap(fig_ica[ind[2], ind[1]], independent_components[:, :, k])
+for (k, ind) in enumerate(CartesianIndices((figs_per_row, size(ica, 3)))[:])
+    if k <= size(ica, 3)
+        heatmap(fig_ica[ind[2], ind[1]], ica[:, :, k])
     end
 end
 display(fig_ica)
 
 pod, i_indices, j_indices = compute_pod_svd(p, 1, 1)
 # This becomes a three-way tensor; the k-th component is given by components[:, :, k]
-proper_components = reshape(pod, (length(i_indices), length(j_indices), size(pod, 2)))
-proper_components = proper_components[:, :, 1:size(independent_components, 3)]
+#proper_components = reshape(pod, (length(i_indices), length(j_indices), size(pod, 2)))
+pod = pod[:, :, 1:size(ica, 3)]
 
 # Plots the independent components
 figs_per_row = 4
 fig_pod = Figure()
-for (k, ind) in enumerate(CartesianIndices((figs_per_row, size(independent_components, 3)))[:])
-    if k <= size(independent_components, 3)
-        heatmap(fig_pod[ind[2], ind[1]], proper_components[:, :, k])
+for (k, ind) in enumerate(CartesianIndices((figs_per_row, size(ica, 3)))[:])
+    if k <= size(ica, 3)
+        heatmap(fig_pod[ind[2], ind[1]], pod[:, :, k])
     end
 end
 display(fig_pod)
